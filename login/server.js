92068a637ba178b4822aba935a3f8c56ffc003d0
kc71486 <linda20030102@gmail.com>
2022-05-05 17:33:48 +0800

    add login/server.js (insert & showall)

--- login/server.js
+++ login/server.js
@@ -15,15 +15,25 @@ const __dirname = dirname(__filename);
 const jsonpath = "./students.json";
 const app = express();
 const port = 1278;
+const identityKey = 'skey';
 
 app.listen(port, () => {
   console.log('listening on port: ' + port);
 })
 
 app.use(express.static(__dirname + '/dist'));
-
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use(session({
+    name: identityKey,//存储在用户cookie中的key名
+    secret: 'aki',  // 用来对session id相关的cookie进行签名
+    store: new FileStore(),  // 本地存储session（文本文件，也可以选择其他store，比如redis的）
+    saveUninitialized: false,  // 是否自动保存未初始化的会话，建议false
+    resave: false,  // 是否每次都重新保存会话，建议false
+    cookie: {
+        maxAge: 10 * 1000  // 有效期，单位是毫秒
+    }
+}));
 
 function saveJSON(src, obj) {
 	let tmp = JSON.stringify(obj), fs = require('fs');
@@ -40,7 +50,13 @@ function loadJSON(src) {
 var allstu = loadJSON(jsonpath);
 
 app.post('/insertUser', (req, res) => {
-	
+	let message = {
+		user: req.body.user,
+		password: req.body.password
+	};
+	allstu[req.body.user] = req.body.password;
+	saveJSON(jsonpath, allstu);
+	res.send("stored: "+JSON.stringify(message));
 });
 
 app.post('/loginUser', (req, res) => {
@@ -48,7 +64,7 @@ app.post('/loginUser', (req, res) => {
 });
 
 app.get('/showAllUser', (req, res) => {
-	
+	res.send(JSON.stringify(allstu));
 });
 
 app.post('/insertWallet', (req, res) => {
