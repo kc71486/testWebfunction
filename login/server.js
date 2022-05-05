ea8be5eed0080d72459041f97978edb2e079b695
kc71486 <linda20030102@gmail.com>
2022-05-06 02:58:48 +0800

    file: login/server.js; location: require area; message: added require crypto; detail: empty;

--- login/server.js
+++ login/server.js
@@ -9,6 +9,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const FileStore = require('session-file-store')(session);
+const crypto = require('crypto');
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
