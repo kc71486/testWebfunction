f23b6c592d006319693cc8256ef2fc2181afd0a2
kc71486 <linda20030102@gmail.com>
2022-05-06 02:22:03 +0800

    file: login/server.js; location: loginUser, logoutUser; message: implemented; detail: not tested;

--- login/server.js
+++ login/server.js
@@ -16,7 +16,7 @@ const jsonpath = "./students.json";
 const app = express();
 const app = express.router();
 const port = 1278;
-const identityKey = 'skey';
+const identityKey = 'sessionid';
 
 app.listen(port, () => {
   console.log('listening on port: ' + port);
@@ -26,13 +26,13 @@ app.use(express.static(__dirname + '/dist'));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(session({
-    name: identityKey,//存储在用户cookie中的key名
-    secret: 'aki',  // 用来对session id相关的cookie进行签名
-    store: new FileStore(),  // 本地存储session（文本文件，也可以选择其他store，比如redis的）
-    saveUninitialized: false,  // 是否自动保存未初始化的会话，建议false
-    resave: false,  // 是否每次都重新保存会话，建议false
+    name: identityKey,//store user's cookie key
+    secret: 'aki',  // session id related cookie signature
+    store: new FileStore(),
+    saveUninitialized: false,
+    resave: false,
     cookie: {
-        maxAge: 10 * 1000  // 有效期，单位是毫秒
+        maxAge: 10 * 1000  // ms
     }
 }));
 
@@ -60,8 +60,61 @@ app.post('/insertUser', (req, res) => {
 	res.send("stored: "+JSON.stringify(message));
 });
 
-app.post('/loginUser', (req, res) => {
-	
+router.post('/loginUser', (req, res) => {
+	//console.log(req);
+	try{
+		let message = {
+			user: req.body.user,
+			password: req.body.password
+		};
+		let response = {
+			success: false,
+			msg: "search failed"
+		};
+		var pattern = /["'=]+/;
+		if(pattern.test(userName)) {
+			response.msg = "illegal attack";
+			res.send(JSON.stringify(response));
+		}
+		/*
+		var sql = 'select * from userInfo where name = "'+message.user+'" and password="'+message.password+'"';
+		console.log(sql);
+		mysql.query(sql,function(err,result,fildes){})
+		*/
+		result = [];
+		for(let i=0; i<allstu.length; i++) {
+			if(allstu[i][0] == message.user && allstu[i][1] == message.password) {
+				result.push(allstu[i]);
+			}
+		}
+		if (result.length == 0) {
+			response.msg = "no such user";
+			res.end(JSON.stringify(response));
+		}else{
+			response.success = true;
+			response.msg = "login successfully";
+			req.session.regenerate(function(err){ //add session info
+				req.session.loginUser =  message.user;
+				res.send(JSON.stringify(response));
+			});
+		}
+		console.log(userName)
+	} catch(e) {
+		 try {
+            callback(false, JSON.stringify(e));
+        } catch (e) {}
+	}
+});
+
+app.get('/logout', function(req, res, next){
+    req.session.destroy(function(err) {
+        if(err){
+            res.end("logout failed");
+            return;
+        }
+        res.clearCookie(identityKey);
+        res.redirect('/');
+    });
 });
 
 app.get('/showAllUser', (req, res) => {
@@ -72,46 +125,11 @@ app.post('/insertWallet', (req, res) => {
 	
 });
 
-router.post('/loginUser',function (req,res){
-	//console.log(req);
-	try{
-		let message = {
-			user: req.body.user,
-			password: req.body.password
-		};
-		let response = {
-			success:false,
-			msg:"查询失败"
-		}
-		var pattern = /["'=]+/;
-		if(pattern.test(userName)){
-			response.msg = "非法攻击";
-			res.end(JSON.stringify(response));
-		}
-		/*
-		var sql = 'select * from userInfo where name = "'+userName+'" and password="'+password+'"';
-		console.log(sql);
-		mysql.query(sql,function(err,result,fildes){})
-		*/
-		console.log(result);
-		 if (result.length == 0) {
-			response.errormsg = "无此人信息";
-			 res.end(JSON.stringify(response));
-		 }else{
-			response.success = true;
-			response.errormsg = "查询成功";
-			var sess = req.session;
-			sess.regenerate(function(err){ //添加session信息
-				req.session.loginUser =  params.userName;
-				 res.end(JSON.stringify(response));
-				
-			})
-			
-		 }
-		console.log(userName)
-	} catch(e) {
-		 try {
-            callback(false, JSON.stringify(e));
-        } catch (e) {}
+app.get('/checkLogin', function(req,res){
+	var logined='';
+	if(!!req.session.loginUser){
+		res.send("currently logined");
+	}else{
+		res.send("not logined");
 	}
 });
