6ace42241e4d11ba84875c9623d07aaededd8bb6
kc71486 <linda20030102@gmail.com>
2022-06-19 00:56:55 +0800

    (websocket) clean code

--- websocket/client/client.js
+++ websocket/client/client.js
@@ -4,45 +4,26 @@ var ws;
 var count = 0; 
 
 document.addEventListener('DOMContentLoaded', (event) => {
-        document.getElementById("startbtn").onclick = (event) => {
-            startWebsocket();
+        document.getElementById('startbtn').onclick = (event) => {
+            establishws();
+        };
+        document.getElementById('sendbtn').onclick = (event) => {
+            establishws();
         };
     });
 
-function startWebsocket() {
-    establishws();
-    /*
-    window.fetch('/startsocket', {
-        method: 'GET'
-    }).then(function(response) {
-        if (response.status >= 200 && response.status < 300)
-            return response;
-        else
-            throw new Error(response.status +":"+response.statusText);
-    }).then(response => response.text()).then(function(response) {
-        console.log(response);
-        establishws();
-    }).catch(function(err) {
-        console.log('Fetch Error :', err);
-    });
-    */
-}
 function establishws() {
     ws = new WebSocket('ws://140.116.177.150:'+wsport+'/');
     ws.addEventListener('open', wsopen);
     ws.addEventListener('close', wsclose);
     ws.addEventListener('message', wsreceive);
-}
+}this.send("count="+count);
 function wsopen() {
     console.log('websocket connected');
 }
 function wsclose() {
     console.log('websocket closed');
 }
-function wsreceive(event) {
-    console.log('recieved: '+event.data);
-    setTimeout(()=>{
-        this.send("count="+count);
-        count += 1;
-    }, 1000);
+function addincre() {
+    ws.send('add incre');
 }
